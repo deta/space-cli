533024ea6fdb99ffc9392dcd27a3f0394e161883
pomdtr <achille.lacoin@gmail.com>
2023-03-15 13:57:58 +0100

    add space dev

--- cmd/dev.go
+++ cmd/dev.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"log"
@@ -22,6 +23,8 @@ import (
 	"github.com/deta/pc-cli/internal/spacefile"
 	"github.com/deta/pc-cli/pkg/components/emoji"
 	"github.com/deta/pc-cli/pkg/components/styles"
+	"github.com/deta/pc-cli/shared"
+	"github.com/phayes/freeport"
 	"github.com/spf13/cobra"
 	"mvdan.cc/sh/v3/shell"
 )
@@ -35,6 +38,7 @@ var (
 		Use:               "dev",
 		Short:             "Run your app locally",
 		PersistentPreRunE: createDataKeyIfNotExists,
+		RunE:              dev,
 	}
 	devUpCmd = &cobra.Command{
 		PreRunE: func(cmd *cobra.Command, args []string) error {
@@ -81,6 +85,7 @@ func init() {
 	// dev
 	devCmd.PersistentFlags().StringP("dir", "d", ".", "directory of the Spacefile")
 	devCmd.PersistentFlags().StringP("id", "i", "", "project id of the project to run")
+	devCmd.Flags().IntP("port", "p", DEV_PORT, "port to run the proxy on")
 	rootCmd.AddCommand(devCmd)
 }
 
@@ -190,7 +195,7 @@ func devUp(cmd *cobra.Command, args []string) (err error) {
 	projectId, _ := cmd.Flags().GetString("id")
 	port, _ := cmd.Flags().GetInt("port")
 	if port == 0 {
-		port, err = GetFreePort()
+		port, err = freeport.GetFreePort()
 		if err != nil {
 			return err
 		}
@@ -205,24 +210,13 @@ func devUp(cmd *cobra.Command, args []string) (err error) {
 		}
 
 		portFile := path.Join(projectDir, ".space", "micros", fmt.Sprintf("%s.port", microName))
-		if _, err := os.Stat(portFile); !errors.Is(err, os.ErrNotExist) {
-			// check if the port is free
-			portStr, err := os.ReadFile(portFile)
-			if err != nil {
-				return err
-			}
+		isRunning, err := isMicroRunning(portFile)
+		if err != nil {
+			return err
+		}
 
-			port, err := strconv.Atoi(string(portStr))
-			if err != nil {
-				return err
-			}
-
-			if _, err = net.DialTimeout("tcp", fmt.Sprintf("localhost:%d", port), 1*time.Second); err == nil {
-				return fmt.Errorf("micro %s is already running", microName)
-			}
-
-			// port is not free, remove the port file
-			os.Remove(portFile)
+		if isRunning {
+			logger.Printf("%s %s is already running on port %d", emoji.Rocket, microName, port)
 		}
 
 		devCommand := micro.Dev
@@ -230,44 +224,15 @@ func devUp(cmd *cobra.Command, args []string) (err error) {
 			devCommand, _ = cmd.Flags().GetString("command")
 		}
 
-		environ := map[string]string{
-			"PORT":                      fmt.Sprintf("%d", port),
-			"DETA_PROJECT_KEY":          projectKey,
-			"DETA_SPACE_APP_HOSTNAME":   fmt.Sprintf("localhost:%d", port),
-			"DETA_SPACE_APP_MICRO_NAME": microName,
-			"DETA_SPACE_APP_MICRO_TYPE": micro.Type(),
+		if err := os.WriteFile(portFile, []byte(fmt.Sprintf("%d", port)), 0644); err != nil {
+			return err
 		}
 
-		fields, err := shell.Fields(devCommand, func(s string) string {
-			if env, ok := environ[s]; ok {
-				return env
-			}
-
-			return os.Getenv(s)
-		})
+		command, err := microCommand(micro, devCommand, projectDir, projectKey, port)
 		if err != nil {
 			return err
 		}
 
-		commandName := fields[0]
-		var commandArgs []string
-		if len(fields) > 0 {
-			commandArgs = fields[1:]
-		}
-
-		command := exec.Command(commandName, commandArgs...)
-		command.Env = os.Environ()
-		for key, value := range environ {
-			command.Env = append(command.Env, fmt.Sprintf("%s=%s", key, value))
-		}
-		command.Dir = path.Join(projectDir, micro.Src)
-		command.Stdout = os.Stdout
-		command.Stderr = os.Stderr
-		command.Stdin = os.Stdin
-
-		if err := os.WriteFile(portFile, []byte(fmt.Sprintf("%d", port)), 0644); err != nil {
-			return err
-		}
 		defer os.Remove(portFile)
 
 		// If we receive a SIGINT or SIGTERM, we want to send a SIGTERM to the child process
@@ -292,17 +257,98 @@ func devProxy(cmd *cobra.Command, args []string) error {
 	routeDir := path.Join(directory, ".space", "micros")
 	spacefile, _ := spacefile.Open(directory)
 
-	routes := make([]proxy.ProxyRoute, 0)
+	reverseProxy, err := proxyFromDir(spacefile.Micros, routeDir)
+	if err != nil {
+		return err
+	}
+
+	log.Println("listening on", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), reverseProxy)
+
+	return nil
+}
+
+func dev(cmd *cobra.Command, args []string) error {
+	projectDir, _ := cmd.Flags().GetString("dir")
+	projectId, _ := cmd.Flags().GetString("id")
+	port, _ := cmd.Flags().GetInt("port")
+
+	routeDir := path.Join(projectDir, ".space", "micros")
+	spacefile, _ := spacefile.Open(projectDir)
+	projectKey, _ := auth.GetProjectKey(projectId)
+
+	// Detect running micros
+	var stoppedMicros []*shared.Micro
 	for _, micro := range spacefile.Micros {
+		portFile := path.Join(routeDir, micro.Name)
+		if isRunning, _ := isMicroRunning(portFile); !isRunning {
+			stoppedMicros = append(stoppedMicros, micro)
+		}
+	}
+
+	freePorts, err := freeport.GetFreePorts(len(stoppedMicros))
+	if err != nil {
+		return err
+	}
+
+	commands := make([]*exec.Cmd, 0, len(stoppedMicros))
+	// Start missing micros
+	for i, micro := range stoppedMicros {
+		command, err := microCommand(micro, "", projectDir, projectKey, freePorts[i])
+		if err != nil {
+			return err
+		}
+		commands = append(commands, command)
+
+		portFile := path.Join(routeDir, fmt.Sprintf("%s.port", micro.Name))
+		os.WriteFile(portFile, []byte(fmt.Sprintf("%d", freePorts[i])), 0644)
+		defer os.Remove(portFile)
+	}
+	proxy, err := proxyFromDir(spacefile.Micros, routeDir)
+	if err != nil {
+		return err
+	}
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: proxy,
+	}
+
+	for _, command := range commands {
+		err := command.Start()
+		// We should kill the other processes if one fails to start
+		if err != nil {
+			return err
+		}
+	}
+
+	// If we receive a SIGINT or SIGTERM, we want to send a SIGTERM to the child process
+	go func() {
+		sigs := make(chan os.Signal, 1)
+		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+		<-sigs
+		for _, command := range commands {
+			command.Process.Signal(syscall.SIGTERM)
+		}
+
+		server.Shutdown(context.Background())
+	}()
+
+	server.ListenAndServe()
+	return nil
+}
+
+func proxyFromDir(micros []*shared.Micro, routeDir string) (*proxy.ReverseProxy, error) {
+	routes := make([]proxy.ProxyRoute, 0)
+	for _, micro := range micros {
 		portFile := path.Join(routeDir, fmt.Sprintf("%s.port", micro.Name))
 		portBytes, err := os.ReadFile(portFile)
 		if err != nil {
-			return err
+			return nil, err
 		}
 
 		microPort, err := strconv.Atoi(string(portBytes))
 		if err != nil {
-			return err
+			return nil, err
 		}
 
 		target, _ := url.Parse(fmt.Sprintf("http://localhost:%d", microPort))
@@ -314,24 +360,72 @@ func devProxy(cmd *cobra.Command, args []string) error {
 		})
 	}
 
-	reverseProxy := proxy.NewReverseProxy(routes)
-	log.Println("listening on", port)
-	http.ListenAndServe(fmt.Sprintf(":%d", port), reverseProxy)
-
-	return nil
+	return proxy.NewReverseProxy(routes), nil
 }
 
-// GetFreePort asks the kernel for a free open port that is ready to use.
-func GetFreePort() (int, error) {
-	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
-	if err != nil {
-		return 0, err
+func isMicroRunning(portFile string) (bool, error) {
+	if _, err := os.Stat(portFile); errors.Is(err, os.ErrNotExist) {
+		return false, nil
+	} else if err != nil {
+		return false, err
 	}
 
-	l, err := net.ListenTCP("tcp", addr)
+	// check if the port is already in use
+	portStr, err := os.ReadFile(portFile)
 	if err != nil {
-		return 0, err
+		return false, err
 	}
-	defer l.Close()
-	return l.Addr().(*net.TCPAddr).Port, nil
+
+	port, err := strconv.Atoi(string(portStr))
+	if err != nil {
+		return false, err
+	}
+
+	if _, err = net.DialTimeout("tcp", fmt.Sprintf("localhost:%d", port), 1*time.Second); err == nil {
+		return true, nil
+	}
+
+	return false, err
+}
+
+func microCommand(micro *shared.Micro, command string, directory, projectKey string, port int) (*exec.Cmd, error) {
+	if command == "" {
+		command = micro.Dev
+	}
+
+	environ := map[string]string{
+		"PORT":                      fmt.Sprintf("%d", port),
+		"DETA_PROJECT_KEY":          projectKey,
+		"DETA_SPACE_APP_HOSTNAME":   fmt.Sprintf("localhost:%d", port),
+		"DETA_SPACE_APP_MICRO_NAME": micro.Name,
+		"DETA_SPACE_APP_MICRO_TYPE": micro.Type(),
+	}
+
+	fields, err := shell.Fields(command, func(s string) string {
+		if env, ok := environ[s]; ok {
+			return env
+		}
+
+		return os.Getenv(s)
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	commandName := fields[0]
+	var commandArgs []string
+	if len(fields) > 0 {
+		commandArgs = fields[1:]
+	}
+
+	cmd := exec.Command(commandName, commandArgs...)
+	cmd.Env = os.Environ()
+	for key, value := range environ {
+		cmd.Env = append(cmd.Env, fmt.Sprintf("%s=%s", key, value))
+	}
+	cmd.Dir = path.Join(directory, micro.Src)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return cmd, nil
 }
