389d5fb3ec473fe22d4006dbf9782401e92a1ceb
pomdtr <achille.lacoin@gmail.com>
2023-03-28 12:18:33 +0200

    update doc generation script

--- scripts/generate-docs.go
+++ scripts/generate-docs.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"os"
 	"strings"
@@ -16,40 +15,47 @@ type Page struct {
 	HeaderOffset int
 }
 
+func buildDoc(command *cobra.Command, headerOffset int) (string, error) {
+	var page strings.Builder
+	err := doc.GenMarkdown(command, &page)
+	if err != nil {
+		return "", err
+	}
+
+	out := strings.Builder{}
+	for _, line := range strings.Split(page.String(), "\n") {
+		if strings.Contains(line, "SEE ALSO") {
+			break
+		}
+		if strings.HasPrefix(line, "#") {
+			if headerOffset < 0 {
+				line = strings.TrimPrefix(line, strings.Repeat("#", -headerOffset))
+			} else {
+				line = strings.Repeat("#", headerOffset) + line
+			}
+		}
+		out.WriteString(line + "\n")
+	}
+
+	for _, child := range command.Commands() {
+		childPage, err := buildDoc(child, headerOffset+1)
+		if err != nil {
+			return "", err
+		}
+		out.WriteString(childPage)
+	}
+
+	return out.String(), nil
+}
+
 func main() {
 	cmd := cmd.NewSpaceCmd()
-	pages := []Page{
-		{Command: cmd, HeaderOffset: -1},
+
+	doc, err := buildDoc(cmd, -1)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error generating docs:", err)
+		os.Exit(1)
 	}
 
-	commands := cmd.Commands()
-	for _, command := range commands {
-		pages = append(pages, Page{Command: command, HeaderOffset: 0})
-	}
-
-	out := bytes.Buffer{}
-	for _, command := range pages {
-		var page strings.Builder
-		err := doc.GenMarkdown(command.Command, &page)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-		for _, line := range strings.Split(page.String(), "\n") {
-			if strings.Contains(line, "SEE ALSO") {
-				break
-			}
-			if strings.HasPrefix(line, "#") {
-				if command.HeaderOffset < 0 {
-					line = strings.TrimPrefix(line, strings.Repeat("#", -command.HeaderOffset))
-				} else {
-					line = strings.Repeat("#", command.HeaderOffset) + line
-				}
-			}
-
-			out.WriteString(line + "\n")
-		}
-	}
-
-	fmt.Println(out.String())
+	fmt.Println(doc)
 }
